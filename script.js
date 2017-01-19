0ec59dcea9ca4a3d79510b9e68ff941dd30720d3
Isabela Biramontes <ibiramontes.student@hsfi.us>
2017-01-19 21:42:30 +0000

    I made a house

---
+++ script.js
@@ -0,0 +1,24 @@
+TinyTurtle.apply(window);
+forward(20);
+right(90);
+left(90)
+right(90);
+forward(100);
+right(90);
+forward(50);
+right(90);
+forward(100);
+right(90);
+forward(50);
+right (45);
+forward(70);
+right(90);
+forward(60);
+
+stamp();
+/*
+forward();
+right();
+left();
+stamp(); 
+*/
